c73b934ac18410759a553a276d0a7d4d4bf0a89f
wanganbang <wanganbang@nationsky.com>
2016-11-08 11:24:21 +0800

    修正pom文件，添加文件上传接口

--- usercenter/src/main/java/com/westar/wab/controller/FilesController.java
+++ usercenter/src/main/java/com/westar/wab/controller/FilesController.java
@@ -1,12 +1,15 @@
 package com.westar.wab.controller;
 
 import com.mongodb.gridfs.GridFSFile;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -23,9 +26,20 @@ public class FilesController {
     @Autowired
     private GridFsTemplate gridFsTemplate;
 
-    @RequestMapping(value = "/upload", method = RequestMethod.POST)
-    public GridFSFile upload(@RequestParam("files") MultipartFile file) throws IOException{
-        GridFSFile fileInfo = gridFsTemplate.store(file.getInputStream(), file.getName(), file.getContentType());
-        return fileInfo;
+    @RequestMapping(value = "/upload", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ResponseBody
+    public String upload(@RequestParam("file") MultipartFile file) throws IOException{
+        System.out.println(file.getSize());
+        GridFSFile fileInfo = gridFsTemplate.store(file.getInputStream(), file.getOriginalFilename(), file.getContentType());
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("filename", fileInfo.getFilename());
+        jsonObject.put("aliases", fileInfo.getAliases());
+        jsonObject.put("uploadDate", fileInfo.getUploadDate().getTime());
+        jsonObject.put("id", fileInfo.getId().toString());
+        jsonObject.put("contentType", fileInfo.getContentType());
+        jsonObject.put("md5", fileInfo.getMD5());
+        jsonObject.put("chunkSize", fileInfo.getChunkSize());
+        jsonObject.put("length", fileInfo.getLength());
+        return jsonObject.toString();
     }
 }
